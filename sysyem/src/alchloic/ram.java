28389dfb3ebef42fc33266d4da0ee81a42955cfb
Meenu <Meenu@LAPTOP-TOE98MBN>
2023-06-26 16:54:04 +0530

    pussing 1st time

---
+++ sysyem/src/alchloic/ram.java
@@ -0,0 +1,18 @@
+package alchloic;
+
+public class ram
+{
+public void m1()
+{
+	System.out.println("this is method 1");
+	}
+public void m2()
+{
+	System.out.println("this is methoid 2");
+	}
+public static void main(String[] args)
+{
+ram obj=new ram();
+obj.m1();
+}
+}
